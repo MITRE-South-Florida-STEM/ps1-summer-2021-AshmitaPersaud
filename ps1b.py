77963cebe196b78fc3c3f6a0aaae958c1645048d
AshmitaPersaud <86687809+AshmitaPersaud@users.noreply.github.com>
2021-07-17 11:23:35 +0000

    Need help with bisection search (part c)

--- ps1b.py
+++ ps1b.py
@@ -1,1 +1,14 @@
-
+annual_salary = float(input("Enter your annual salary: "))
+portion_saved = float(input("Enter the percent of your salary to save, as a decimal: "))
+total_cost = float(input("Enter the cost of your dream home: "))
+semi_annual_raise = float(input("Enter the semi-annual raise, as a decimal: "))
+current_savings = 0
+r = .04
+months = 0
+portion_down_payment = total_cost*0.25
+while current_savings < portion_down_payment:
+  current_savings += portion_saved*(annual_salary/12)+ current_savings*(r/12)
+  months += 1
+  if months%6 == 0:
+   annual_salary += annual_salary*semi_annual_raise
+print("Number of months:", months)
--- ps1c.py
+++ ps1c.py
@@ -1,1 +1,34 @@
+annual_salary  = float(input('Enter the starting salary: '))
+total_cost = 1000000.0
 
+semi_annual_raise = .0700
+current_savings = 0
+r = .0400
+months = 36
+steps = 0
+low = 0.0
+high = 1000.0
+guess_rate = (low+high)/2.000
+portion_down_payment = total_cost*0.25
+monthly_salary = annual_salary/12
+while abs(current_savings - portion_down_payment) >= 100:
+    current_savings = 0
+    rate = guess_rate/10000
+    for month in range(36):
+      if months % 6 == 0 and months > 0:
+        annual_salary += annual_salary*semi_annual_raise
+        monthly_salary = annual_salary/12
+        current_savings += monthly_salary*rate+current_savings*r/12
+      if current_savings < portion_down_payment:
+         low = guess_rate
+      else:
+        high = guess_rate
+    guess_rate = (high + low)//2
+    steps += 1
+    if current_savings > portion_down_payment:
+        break
+    if current_savings > portion_down_payment:
+     print('It is not possible to pay the down payment in three years.')
+    else:
+     print('Best savings rate:', rate)
+     print('Steps in bisection search:', steps)
